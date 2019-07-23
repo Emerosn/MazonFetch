328a25ae1d93941568a9062de07e74e3a6ad19b2
Emerosn <31738756+Emerosn@users.noreply.github.com>
2019-07-23 09:36:39 -0300

    Update mazonfetch.sh
    Mudanças na logo

--- mazonfetch.sh
+++ mazonfetch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Template orgulhosamente criado por (Shell-Base)
-#═════-HEADER════════════════════════════════-|
+#═════-HEADER═════════════════════════════════|
 #AUTOR
 #  Emerson Alves <emerson_alves262016@hotmail.com>
 #
@@ -58,7 +58,7 @@
 # yellow
 #═════════════>
 
-#════FUNÇÕES═════->
+#════FUNÇÕES═════>
 # _fetch_info()
 # _set_info()
 # _mazon()
@@ -66,8 +66,10 @@
 # _inf=()
 
 #═════════════>
-#═══TESTES═══════>
-
+#═══inicializando═══════>
+nvidia=$(which nvidia-settings 2>&1 > /dev/null &)
+gpu=$(awk '{print $2}' <<< $(grep "Vendor" <<< $(glxinfo)))
+amd=$(awk '{print $2}' <<< $(grep "Vendor" <<< $(glxinfo)))
 #═════════════>
 
 #Inicio do Programa
@@ -89,12 +91,11 @@ if tput setaf 1 &> /dev/null; then
 	yellow=$(tput setaf 136);
 
 else
-
 	bold='';
 	reset="\e[0m";
 	violet="\e[1;30m";
 	blue="\e[1;34m";
-	cyan="\e[1;36m";
+	cyan= "\e[1;36m";
 	green="\e[1;32m";
 	orange="\e[1;33m";
 	purple="\e[1;35m";
@@ -114,8 +115,9 @@ _fetch_info(){
 	f_os=$(uname -o)
 	f_kname=$(awk '{print $1}' <<< $(uname -a))
 	f_kv=$( uname -a | awk '{print $3}')
-	f_uptime=$(tr "," " " <<< $(awk '{print $3, $2}' <<< $(uptime)))
+#	f_uptime=$(tr "," " " <<< $(awk '{print $3, $2}' <<< $(uptime)))
 #	f_uptime=$(uptime | awk '{print $1, $2}')
+	f_uptime=$( tr ' ' ': '<<< $(awk '{print $3,$5 $2}' <<< $(uptime)))
 	f_shell=$(basename $SHELL)
 	f_resolucao=$(paste - - <<< $(awk '/mode / {print $10}' <<< $(grep -E "Output .* mode" < "/var/log/Xorg.0.log")))
 	f_desk=$XDG_CURRENT_DESKTOP
@@ -123,12 +125,8 @@ _fetch_info(){
 	f_cpu=$(sed 's/.*:.//g;s/(.)//g;s/@//g;s/CPU//g;s/(.*)//g;s/  / /g;' <<< $(sed -n '1p' <<< $(grep -o 'model name.*' < "/proc/cpuinfo")))
 	f_tcpu=$(sed 's/.*:  +//g;s/ .*//' <<< $(grep "Package id 0:" <<< $(sensors)))
 	f_gpu=$(
-
-			nvidia=$(which nvidia-settings 2>&1 > /dev/null &)
-			gpu=$(awk '{print $2}' <<< $(grep "Vendor" <<< $(glxinfo)))
-			amd=$(awk '{print $2}' <<< $(grep "Vendor" <<< $(glxinfo)))
 		if [ -x "$nvidia" ]; then
-            sed 's/.*GPU //g;s/(.*)//' <<< $(grep "NVIDIA(0): NVIDIA" < "/var/log/Xorg.0.log")
+            		sed 's/.*GPU //g;s/(.*)//' <<< $(grep "NVIDIA(0): NVIDIA" < "/var/log/Xorg.0.log")
 		elif [ "$gpu" == "Intel" ]; then
 			sed 's/.*: //'<<< $(grep "Integrated Graphics Chipset:" < "/var/log/Xorg.0.log")
  
@@ -139,8 +137,8 @@ _fetch_info(){
 		fi
 	)
 
-	f_mem=$(bc <<< $(printf "%i"$(tr -d [A-Za-z:' '] <<< $(sed -n '1p' < "/proc/meminfo"))/1024))" MB"
-	f_memfree=$(bc <<< $(printf "%i"$(tr -d [A-Za-z:' '] <<< $(sed -n '2p' < "/proc/meminfo"))/1024))" MB"
+	f_mem=$(bc <<< $(echo "scale=1;$(bc <<< $(printf "%i"$(tr -d [A-Za-z:' '] <<< $(sed -n '1p' < "/proc/meminfo"))/1000))/1024"))" GB"
+	f_memfree=$(bc <<< $(echo "scale=1;$(bc <<< $(printf "%i"$(tr -d [A-Za-z:' '] <<< $(sed -n '3p' < "/proc/meminfo"))/1000))/1024"))" GB"
 	f_ach=$(getconf LONG_BIT)"-bit"
 	f_nave=$(sed 's/userapp-//;s/\..*//g'<<< $(xdg-settings get default-web-browser))
 	f_char=$(expr length "$f_title") ql=
@@ -152,107 +150,53 @@ _fetch_info(){
 
 _set_info()
 {
-    _fetch_info
-	_inf[0]="$(printf "%38s" " ")${red}╔══════════════════════════════════════════════════╗${reset}"
-	_inf[1]="║$(printf "%6s" " ")${green}$f_title${reset}"
-	_inf[2]="║$(printf "%6s" " ")${red}$ql${reset}"
-	_inf[3]="║${blue}Distro     ${yellow}:${yellow} $f${reset}"
-	_inf[4]="║${blue}OS         ${yellow}:${yellow} $f_os${reset}"
-	_inf[5]="║${blue}Kernel Name${yellow}:${yellow} $f_kname${reset}"
-	_inf[6]="║${blue}Release    ${yellow}:${yellow} $f_kv${reset}"
-	_inf[7]="║${blue}Architetura${yellow}:${yellow} $f_ach${reset}"
-	_inf[8]="${red}╠══════════════════════════════════════════════════${reset}"
-	_inf[9]="║${blue}Mem RAM    ${yellow}:${yellow} $f_mem${reset}"
-	_inf[10]="║${blue}Mem free   ${yellow}:${yellow} $f_memfree${reset}"
-	_inf[11]="║${blue}Uptime     ${yellow}:${yellow} ${f_uptime}${reset}"
-	_inf[12]="║${blue}Shell      ${yellow}:${yellow} $f_shell${reset}"
-	_inf[13]="${red}╠══════════════════════════════════════════════════${reset}"
-	_inf[14]="║${blue}CPU        ${yellow}:${yellow} $f_cpu [$f_tcpu]${reset}"
-	_inf[15]="║${blue}Core(s)    ${yellow}:${yellow} $(nproc)${reset}"
-	_inf[16]="║${blue}GPU        ${yellow}:${yellow} $f_gpu${reset}"
-	_inf[17]="${red}╠══════════════════════════════════════════════════${reset}"
-	_inf[18]="║${blue}Desktop    ${yellow}:${yellow} $f_desk${reset}"
-	_inf[19]="║${blue}Fonte      ${yellow}:${yellow} $f_font${reset}"
-	_inf[20]="║${blue}Resolution ${yellow}:${yellow} $f_resolucao${reset}"
-	_inf[21]="$(printf "%53s" " ")║${blue}Browser    ${yellow}:${yellow} $f_nave${reset}"
-	_inf[22]="$(printf "%53s" " ")${red}╚══════════════════════════════════════════════════╝${reset}"
+	_fetch_info
+	_inf[0]="${bold}${green}$f_title${reset}"
+	_inf[1]="${bold}${red}$ql${reset}"
+	_inf[2]="${bold}${white}Distro     ${white}:${white} $f${reset}"
+	_inf[3]="${bold}${white}OS         ${white}:${white} $f_os${reset}"
+	_inf[4]="${bold}${white}Kernel Name${white}:${white} $f_kname${reset}"
+	_inf[5]="${bold}${white}Release    ${white}:${white} $f_kv${reset}"
+	_inf[6]="${bold}${white}Architetura${white}:${white} $f_ach${reset}"
+	_inf[7]="${white}Mem RAM    ${white}:${white} $f_mem${reset}"
+	_inf[8]="${white}Mem free   ${white}:${white} $f_memfree${reset}"
+	_inf[9]="${white}Uptime     ${white}:${white} ${f_uptime}${reset}"
+	_inf[10]="${white}Shell      ${white}:${white} $f_shell${reset}"
+	_inf[11]=${bold}"${white}CPU        ${white}:${white} $f_cpu [$f_tcpu]${reset}"
+	_inf[12]=${bold}"${white}Core(s)    ${white}:${white} $(nproc)${reset}"
+	_inf[13]="${bold}${white}GPU        ${white}:${white} $f_gpu${reset}"
+	_inf[14]="${bold}${white}Desktop    ${white}:${white} $f_desk${reset}"
+	_inf[15]="${white}Fonte      ${white}:${white} $f_font${reset}"
+	_inf[16]="${white}Resolution ${white}:${white} $f_resolucao${reset}"
+	_inf[17]="${white}Browser    ${white}:${white} $f_nave${reset}"
+
 
 }
+_mazon(){
+	_arr[0]="${red}´´´´´´´´´´´´´´´´´´´´´´´${reset}"
+	_arr[1]="${red}´´´´´´´´´´´´´´´´´´´´´´´${reset}"
+	_arr[2]="${red}´´´´´´´´´´´´´´´´´´´´´´´${reset}"
+	_arr[3]="${red}´´´´´´´´´´´´´´´´´´´´´´´${reset}"
+	_arr[4]="${red}´´´´´´´´´´´´´´´´´´´´´´´${reset}"
+	_arr[5]="${red}´´´´´´´´´´´´´´´´´´´´´´´${reset}"
+	_arr[6]="${red}´´´´´´´´´´´´´´´´´´´´´´´${reset}"
+	_arr[7]="${red}´´´´´´´´´${reset}${bold}${green}__${reset}${red}´´´´´´´´´´´´${reset}"
+	_arr[8]="${red}´´´´´´´´${reset}${bold}${green}|  |_${reset}${red}´´´´´´´´´´${reset}"
+	_arr[9]="${red}´´´´´´´´´${reset}${bold}${green}--  |${reset}${red}´´´´´´´´´${reset}"
+	_arr[10]="${red}´´´´´´´´´´´${reset}${bold}${green}´´${reset}${red}´´´´´´´´´´${reset}"
+	_arr[11]="${red}´´´´´´´´´´´´´´´´´´´´´´´${reset}"
+	_arr[12]="${red}´´´´´´´´´´´´´´´´´´´´´´´${reset}"
+	_arr[13]="${red}´´´´´´´´´´´´´´´´´´´´´´´${reset}"
+	_arr[14]="${red}´´´´´´´´´´´´´´´´´´´´´´´${reset}"
+	_arr[15]="${red}´´´´´´´´´´´´´´´´´´´´´´´${reset}"
+	_arr[16]="${red}´´´´´´´´´´´´´´´´´´´´´´´${reset}"
+	_arr[17]="${red}´´´´´´´´´´´´´´´´´´´´´´´${reset}"
 
-#_mazon(){
-#_arr[0]="$(printf "%s" "")"
-#_arr[1]="                                      "
-#_arr[2]="                                      "
-#_arr[3]="${green}            ::cccclllccc::            ${reset}"
-#_arr[4]="${green}        :cllllllllllllllllllc:        ${reset}"
-#_arr[5]="${green}      :l${white}OKo${green}llllllllllllllllllllc      ${reset}"
-#_arr[6]="${green}    :lll${white}xMWKko${green}lllllllllllllllllll:    ${reset}"
-#_arr[7]="${green}   cllll${white}oMMMMMNKOxo${green}lllllllllllllllc   ${reset}"
-#_arr[8]="${green}  cllllll${white}XMM${green}WXN${white}MMMMNXOxo${green}lllllllllllc  ${reset}"
-#_arr[9]="${green} :lllllll${white}xMMMM${green}WKKKN${white}MMMMMNKxo${green}llllllll: ${reset}"
-#_arr[10]="${green} cllllllll${white}KMMMMMM${green}WKOk0W${white}MMMMWOo${green}lllllll ${reset}"
-#_arr[11]="${green} lllllllll${white}oXMMMMMMMMM${green}XkxK${white}MMMMNd${green}llllll ${reset}"
-#_arr[12]="${green} clllllllll${white}oXMMMMMMMMMM${green}WOdK${white}MMMNo${green}llllc ${reset}"
-#_arr[13]="${green} :llllllllll${white}oKMMMMMMMMMMM${green}XoO${white}MMMx${green}llll: ${reset}"
-#_arr[14]="${green}  clllllllllll${white}xNMMMMMMMMMM${green}0lK${white}MMd${green}lllc  ${reset}"
-#_arr[15]="${green}   :llllllllllll${white}kXMMMMMMMM${green}Wox${white}Wk${green}lllc   ${reset}"
-#_arr[16]="${green}    :cllllllllllll${white}dOXWMMMMM${green}ox${white}o${green}lll:    ${reset}"
-#_arr[17]="${green}      :cllllllllllllll${white}oxkOW${green}oxl${green}c:      ${reset}"
-#_arr[18]="${green}         :clllllllllllllll${white}0c          ${reset}"
-#_arr[19]="${green}             :::cccccc:::             ${reset}"
-
-
-#_mazon(){
-#_arr[0]="    "
-#_arr[1]="${green}	       ,';;;;;;;;;                ${reset}"
-#_arr[2]="${green}         .',,,;;;;;;;     ,,,,,'.         ${reset}"
-#_arr[3]="${green}      .,;;;;;;,,;;;    ;;;;;;;;;;,'.      ${reset}"
-#_arr[4]="${green}    .,;;;;;;;;;;;;;,''...',,;,,'.....     ${reset}"
-#_arr[5]="${green}  .,;;;;;;;;;;;,..                        ${reset}"
-#_arr[6]="${green}     ';;;;;;;,.                  .,;;;;,  ${reset}"
-#_arr[7]="${green}  ;;;;;;;;;;,.                     ';;;;; ${reset}"
-#_arr[8]="${green}';;;;;;;;;;;.                       ,;;;;'${reset}"
-#_arr[9]="${green},;;;;;;;;;;;      ${white}.''.      cONMWO:  ${green},;;;;${reset}"
-#_arr[10]="${green},;;;;;;;;;;,    ${white}xWMMMMNkc,kMMMMKNMMW,${green}.;;;;${reset}"
-#_arr[11]="${green}.;;;;;;;;;;,   ${white}dMMMWXMMMMMMMMM0 .WMM0 ${green},;;'${reset}"
-#_arr[12]="${green} ,;;;;;;;;;,   ${white}OMMM' xMMMMMMMMMMMMMMN ${green},;; ${reset}"
-#_arr[13]="${green}  ,;;;;;,..    ${white}:MMMMMMMMMN${yellow}K00K${white}NMMMMMN..'  ${reset}"
-#_arr[14]="${green}   ';;    ''   ${white}.KXWMMMMMX${yellow}xxxxxx${white}WMMMN0.    ${reset}"
-#_arr[15]="${green}       .,;;,.   ${white}xKKXNMMMK${yellow}dxxxxx${white}XMWKx;     ${reset}"
-#_arr[16]="${green}        ......  ${white}.ok0KKXNWc${yellow}lxxxx${white}Okc'.      ${reset}"
-#_arr[17]="${green}                 ${white}.',cdkOKd,;${yellow}oxx${white}c.         ${reset}"
-#_arr[18]="${green}                      ${white}....   ${yellow}.d.          ${reset}"
-#_arr[19]="                                          "
-#_arr[20]="    "
-#_arr[21]="    "
-#_arr[22]="    "
-
-_mazon(){ 
-_arr[0]="               "
-_arr[1]="${white}                   lXMNd                 ${white}            ${reset}"
-_arr[2]="${white}     .cdxdl'    .xWMMMMMWo                 ${white}          ${reset}"
-_arr[3]="${white}     0MMMMMMMWOcOMMMMM0NMMMX                ${white}         ${reset}"
-_arr[4]="${white}    lMMMMMMMMMMMMMMMMX  MMMM'               ${white}         ${reset}"
-_arr[5]="${white}    dMMMM  MMMMMMMMMMMMMMMMMo               ${white}         ${reset}"
-_arr[6]="${white}    ;MMMMWWMMMMMk${yellow}WNNW${white}MMMMMMMM0               ${white}        ${reset}"
-_arr[7]="${white}     .cKMMMMMMM${yellow}0xxxxx${white}0MMMMMMK                        ${reset}"
-_arr[8]="${white}        .OMMMMN${yellow}xxxxxxx${white}MMMMN0${green}:,,,,..                  ${reset}"
-_arr[9]="${green}   ...... ${white}xMMMWO${yellow}xxxxxx${white}NWKx${green}:,,;,...'',,;;;;,.         ${reset}"
-_arr[10]="${green} ..........${white}WMMMMXk${yellow}xxxx${white}c\033${green}x,,;;;;;;;;;;;;,'.....        ${reset}"
-_arr[11]="${green} ......... :k${white}NMMMWk${yellow}xxx${green}:''..;;;;;;;;;;;'...''''.      ${reset}"
-_arr[12]="${green} ...... .....':do;',d${yellow}x${white}${green}, .',;;;;;;;;;;;;;;;;;;;;,.    ${reset}"
-_arr[13]="${green}               ',...  ;'';;;;;;;;;;;;;;;;;;;;;;;;;'  ${reset}"
-_arr[14]="${green}             ,;,..  .,;;;;;;;;;;;;;;;;;;;;;;;'..     ${reset}"
-_arr[15]="${green}              ..   .;;;;;;;;;;;,'...;;;;;;;;;;;,.    ${reset}"
-_arr[16]="${green}                   ;;;;;;;;;;'.  .,;;;;;;;;;;;;;;.   ${reset}"
-_arr[17]="${green}                    .';;;;;,  .,;;;;;;;;;;;;;;;;,    ${reset}"
-_arr[18]="${green}                       ..'.  .'',,,;;;;;;;;;;;;;;    ${reset}"
-_arr[19]="${green}                                     ...',;;;;;;;.   ${reset}"
-_arr[20]="${green}                                           ..,,,'    ${reset}"
-	for i in $(seq 0 23);
+	for i in $(seq 0 17);
 	do
 		printf "%s\n%s""${_arr[$i]}  ${_inf[$i]}"
 	done
+echo -e "\n"
 }
 _set_info
 _mazon
